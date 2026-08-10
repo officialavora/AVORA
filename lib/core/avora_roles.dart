0153f5df2687712b3202bf64233713d40bd8e599
officialavora <officialavora7@gmail.com>
2026-08-10 13:46:46 +0000

    Add future-ready AVORA role structure

--- lib/core/avora_roles.dart
+++ lib/core/avora_roles.dart
@@ -16,10 +16,10 @@ enum AvoraCommerceRole {
 
 enum AvoraStaffAssignment {
   none,
-  countryManager,
-  csHead,
   customerService,
+  csHead,
   eventOrganizer,
+  countryManager,
 }
 
 enum AvoraScopeType {
@@ -31,48 +31,47 @@ enum AvoraScopeType {
   global,
 }
 
+extension AvoraAuthorityRoleX on AvoraAuthorityRole {
+  String get title {
+    switch (this) {
+      case AvoraAuthorityRole.owner:
+        return 'Owner';
+      case AvoraAuthorityRole.manager:
+        return 'Manager';
+      case AvoraAuthorityRole.superAdmin:
+        return 'Super Admin';
+      case AvoraAuthorityRole.admin:
+        return 'Admin';
+      case AvoraAuthorityRole.bd:
+        return 'BD';
+      case AvoraAuthorityRole.agency:
+        return 'Agency';
+      case AvoraAuthorityRole.user:
+        return 'User';
+    }
+  }
+}
+
 class AvoraRoleStructure {
-  static const Set<AvoraAuthorityRole> launchRoles = {
-    AvoraAuthorityRole.manager,
-    AvoraAuthorityRole.bd,
-    AvoraAuthorityRole.agency,
-    AvoraAuthorityRole.user,
+  static const Map<AvoraAuthorityRole, int> authorityRank = {
+    AvoraAuthorityRole.user: 0,
+    AvoraAuthorityRole.agency: 10,
+    AvoraAuthorityRole.bd: 20,
+    AvoraAuthorityRole.admin: 30,
+    AvoraAuthorityRole.superAdmin: 40,
+    AvoraAuthorityRole.manager: 50,
+    AvoraAuthorityRole.owner: 100,
   };
 
-  static const Map<AvoraAuthorityRole, Set<AvoraAuthorityRole>> canAssign = {
-    AvoraAuthorityRole.owner: {
-      AvoraAuthorityRole.manager,
-      AvoraAuthorityRole.superAdmin,
-      AvoraAuthorityRole.admin,
-      AvoraAuthorityRole.bd,
-      AvoraAuthorityRole.agency,
-      AvoraAuthorityRole.user,
-    },
-    AvoraAuthorityRole.manager: {
-      AvoraAuthorityRole.superAdmin,
-      AvoraAuthorityRole.admin,
-      AvoraAuthorityRole.bd,
-      AvoraAuthorityRole.agency,
-      AvoraAuthorityRole.user,
-    },
-    AvoraAuthorityRole.superAdmin: {
-      AvoraAuthorityRole.admin,
-      AvoraAuthorityRole.bd,
-      AvoraAuthorityRole.agency,
-      AvoraAuthorityRole.user,
-    },
-    AvoraAuthorityRole.admin: {
-      AvoraAuthorityRole.bd,
-      AvoraAuthorityRole.agency,
-      AvoraAuthorityRole.user,
-    },
-    AvoraAuthorityRole.bd: {
-      AvoraAuthorityRole.agency,
-      AvoraAuthorityRole.user,
-    },
-    AvoraAuthorityRole.agency: {
-      AvoraAuthorityRole.user,
-    },
-    AvoraAuthorityRole.user: {},
-  };
+  static bool isHigherAuthority(
+    AvoraAuthorityRole actor,
+    AvoraAuthorityRole target,
+  ) {
+    return (authorityRank[actor] ?? 0) > (authorityRank[target] ?? 0);
+  }
+
+  static bool managerCanAssign(AvoraAuthorityRole target) {
+    return target != AvoraAuthorityRole.owner &&
+        target != AvoraAuthorityRole.manager;
+  }
 }
